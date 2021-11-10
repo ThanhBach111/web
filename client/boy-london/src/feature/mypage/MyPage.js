57d433f983e3101a46e3634d4f534d811cfe9b4f
duongyp23 <71799659+duongyp23@users.noreply.github.com>
2021-11-10 22:39:50 +0700

    Add files via upload

---
+++ client/boy-london/src/feature/mypage/MyPage.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./mypage.css";
+import StyleInput from "../../components/StyleInput";
+
+
+const info = [
+    {
+      id: 0,
+      image:"/img/arrival1.png",
+      name: "Product",
+      
+    },
+    {
+      id: 1,
+      image:"/landingpage/img/arrival1.png",
+      name: "Product",
+      
+    },
+    {
+      id: 2,
+      image:'/img/arrival3',
+      name: "Product",
+      
+    },
+    {
+      id: 3,
+      image:'/img/arrival4',
+      name: "Product",
+      
+    },
+    
+  ];
+
+const MyPage = () => {
+    return(
+        <div className="page">
+            <div className="userinfo">
+                <h1>Tài khoản của tôi</h1>
+                {info.map((item) => (
+                <StyleInput image={item.image} name={item.name}  />
+                ))}
+                <button>Cập nhật thông tin</button>
+            </div>
+            <div className="cartinfo">
+              <h1>Lịch sử mua hàng</h1>
+              <table>
+                <tr>
+                  <th>Mã đơn hàng</th>
+                  <th>Ngày mua</th>
+                  <th>Sản phẩm</th>
+                  <th>Tổng tiền</th>
+                </tr>
+              </table>
+              <h1>Thông tin khuyến mãi</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default MyPage;
