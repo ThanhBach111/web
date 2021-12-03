2f355e30ca1eb9e8298ee87f2731b115caedf0b4
duongyp23 <71799659+duongyp23@users.noreply.github.com>
2021-12-03 21:50:04 +0700

    Update LandingPage.js

--- client/boy-london/src/feature/landingpage/LandingPage.js
+++ client/boy-london/src/feature/landingpage/LandingPage.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { ROOT_SCREEN } from "../../navigation/routes";
 import StyleProduct from "../../components/StyleProduct";
 import Images from "../../assets/images";
 
-const data = [
+const newProduct = [
     {
       id: 0,
       image:  "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
@@ -30,32 +30,90 @@ const data = [
     },
     
   ]
-const LandingPage = () => {
+  const bestSaleProduct = [
+    {
+      id: 0,
+      image:  "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+      name: "Product",
+      price: "200,000d",
+    },
+    {
+      id: 1,
+      image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+      name: "Product",
+      price: "200,000d",
+    },
+    {
+      id: 2,
+      image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+      name: "Product",
+      price: "200,000d",
+    },
+    {
+      id: 3,
+      image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+      name: "Product",
+      price: "200,000d",
+    },
+    
+  ]
+  const Banner ="https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg" 
 
+  const Banner2 ="https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg" 
+
+const ProductStyle = [
+  {
+      image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+  },
+  {
+    image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+  },
+  {
+    image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+  },
+  {
+    image: "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
+  }
+]
+
+const LandingPage = () => {
+  const openListProduct = () => {
+    window.location.href = ROOT_SCREEN.shop;
+  };
     return(
         <>
         <div style={Style.page}>
-            <div style={Style.banner}></div>
-            <div style={Style.style}>
-                
+            <div style={Style.banner}>
+              <img src={Banner} style={Style.imageStyle}></img>
             </div>
-            <div style={Style.text}><b>NEW ARRIVAL</b></div>
-            <div style={Style.text}>show more</div>
+            <div style={Style.style}>
+            {ProductStyle.map((item) => (
+                <img src={item.image} style={Style.imageStyle2}/>
+                ))}
+            </div>
+            <div style={Style.text}>
+              <p><b>NEW ARRIVAL</b></p>
+              <button style={Style.showmore} onClick={openListProduct}>show more</button>
+            </div>
             <div style={Style.newarrival}>
-                {data.map((item) => (
+                {newProduct.map((item) => (
                 <StyleProduct image={item.image} name={item.name} price={item.price} />
                 ))}
             </div>
             
-            <div style={Style.text}><b>New Product</b></div>
-            <div style={Style.text} >show more</div>
-            <div Style={Style.bestsale}>
+            <div style={Style.text}>
+              <p><b>Best Sale Product</b></p>
+              <button style={Style.showmore} onClick={openListProduct}>show more</button>
+            </div>
+            <div style={Style.bestSale}>
               <div style={Style.bestsaleproduct}>
-                  {data.map((item) => (
+                  {bestSaleProduct.map((item) => (
                   <StyleProduct image={item.image} name={item.name} price={item.price} />
                   ))}
               </div>
-              <div Style={Style.imgview}></div>
+              <div style={Style.imgView}>
+              <img src={Banner2} style={Style.imageStyle}></img>
+              </div>
             </div>
             <div style={Style.contact}>
               <div style={Style.contactblock}>
@@ -84,6 +142,18 @@ const LandingPage = () => {
 }
 
 const Style = {
+  imageStyle: {
+    width: "100%",
+    height: "100%"
+  },
+  showmore: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    marginLeft : "auto",
+    marginRight: "auto",
+    fontSize: "x-large",
+    textDecoration: "underline",
+  },
   page: {
     display: "block",
 position: "relative",
@@ -92,15 +162,22 @@ position: "relative",
     backgroundSize: "cover"
 },
 banner: { 
-    width: "1440px",
-    height: "538px",
+    width:1400,
+    height: 538,
+    
 },
 style: {
   marginTop: 50,
-  width: "100%",
-  height: 489,
+  height: 450,
   display: "flex",
   flexDirection: "row"
+},
+imageStyle2 :{
+  marginTop: 5,
+  marginBottom: 5,
+  marginLeft: 5,
+  marginRight: 5,
+  width: 340
 },
 text: {
   margin: 10,
@@ -113,21 +190,23 @@ newarrival: {
   display: "flex",
   flexWrap: "wrap",
 },
-bestsale: {
+bestSale: {
+  width: "100%",
   height: 1100,
   display: "flex",
-  flexWrap: "wrap",
+  flexDirection: "row"
 },
 bestsaleproduct: {
-  width: "50%",
+  width: 700,
   height: 1100,
   display: "flex",
   flexWrap: "wrap"
 },
-imgview: {
-  width: "50%",
-  height: 1100,
-  backgroundSize: "cover"
+imgView: {
+  width: 680,
+  height: 1050,
+  marginLeft: 10,
+  marginRight: 10
 },
 contact: {
   paddingLeft: 10,
