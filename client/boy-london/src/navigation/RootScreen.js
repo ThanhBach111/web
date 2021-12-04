9ce01bf3f3fdc07f4bc09226db8a72cc371177e0
Duc Dat <user@usernoMBP.lan>
2021-12-04 16:21:04 +0700

    fixbug/fix_register

--- client/boy-london/src/navigation/RootScreen.js
+++ client/boy-london/src/navigation/RootScreen.js
@@ -6,7 +6,6 @@ import ProductDetail from "../feature/product/ProductDetail";
 import NavBar from "./NavBar";
 import { ADMIN_ROUTE, ROOT_SCREEN, SHOP_ROUTE, USER_ROUTE } from "./routes";
 import LandingPage from "../feature/landingpage/LandingPage";
-import Register from "../feature/user/Register";
 import Login from "../feature/user/Login";
 import ForgotPass from "../feature/user/ForgotPass";
 import Manager from "../feature/manage/Manager";
@@ -14,7 +13,7 @@ import MyPage from "../feature/mypage/MyPage";
 import Instruction from "../feature/user/Instruction";
 import Policy from "../feature/user/Policy";
 import PolicyHomePage from "../feature/user/PolicyInstruct";
-
+import Register from "../feature/user/Register";
 
 const RootScreen = () => {
   return (
@@ -27,14 +26,13 @@ const RootScreen = () => {
         <Route path={SHOP_ROUTE.productDetail} component={ProductDetail} />
         <Route path={SHOP_ROUTE.cartPayment} component={CartPayment} />
         <Route path={ROOT_SCREEN.login} component={Login} />
-        <Route path={USER_ROUTE.register} component={Register}/>
-        <Route path={USER_ROUTE.forgotPass} component={ForgotPass}/>
-        <Route path={ADMIN_ROUTE.productManage} component={Manager}/>
+        <Route path={USER_ROUTE.register} component={Register} />
+        <Route path={USER_ROUTE.forgotPass} component={ForgotPass} />
+        <Route path={ADMIN_ROUTE.productManage} component={Manager} />
         <Route path={USER_ROUTE.myPage} component={MyPage} />
         <Route path={USER_ROUTE.instruction} component={Instruction} />
         <Route path={USER_ROUTE.policy} component={Policy} />
         <Route path={USER_ROUTE.policyInstruc} component={PolicyHomePage} />
-      
       </Switch>
     </BrowserRouter>
   );
