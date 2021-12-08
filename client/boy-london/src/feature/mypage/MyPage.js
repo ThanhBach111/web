dce0e25bf36aa5f6cb2bbeca4b806e7272f0d7d9
Duc Dat <user@usernoMBP.lan>
2021-12-08 21:42:20 +0700

    fixbug/my_page

--- client/boy-london/src/feature/mypage/MyPage.js
+++ client/boy-london/src/feature/mypage/MyPage.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import StyleInput from "../../components/StyleInput";
 import Images from "../../assets/images";
 import { ADMIN_ROUTE } from "../../navigation/routes";
+import request from "../../api/request";
 
 const listPromotion = [
-  
   "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
 
   "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
@@ -12,7 +12,6 @@ const listPromotion = [
   "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
 
   "https://media.vov.vn/sites/default/files/styles/large/public/2021-02/p21_0055_a5_rgb.jpg",
-
 ];
 
 const MyPage = () => {
@@ -22,13 +21,11 @@ const MyPage = () => {
   const [address, setAddress] = useState("");
   const getData = async () => {
     try {
-      
-      const res = await request.get("/account/");
+      const res = await request.get("/account");
       setName(res.name);
       setEmail(res.mail);
       setPhone(res.phone);
       setAddress(res.address);
-     
     } catch (err) {
       console.log(err);
     }
@@ -55,87 +52,93 @@ const MyPage = () => {
     window.location.href = ADMIN_ROUTE.productManage;
   };
 
-    return(
-        <div style={Style.page}>
-            <div style={Style.userinfo}>
-                <h1>Tài khoản của tôi</h1>
-                  <StyleInput
-                    icon={Images.userText}
-                    placeholder="Họ và tên"
-                    value={name}
-                    setValue={setName}
-                  />
+  return (
+    <div style={Style.page}>
+      <div style={Style.userinfo}>
+        <h1>Tài khoản của tôi</h1>
+        <StyleInput
+          icon={Images.userText}
+          placeholder="Họ và tên"
+          value={name}
+          setValue={setName}
+        />
 
-                  <div style={{ marginTop: 40 }}></div>
-                  <StyleInput
-                    icon={Images.mail}
-                    placeholder="Email"
-                    value={email}
-                    setValue={setEmail}
-                  />
+        <div style={{ marginTop: 40 }}></div>
+        <StyleInput
+          icon={Images.mail}
+          placeholder="Email"
+          value={email}
+          setValue={setEmail}
+        />
 
-                  <div style={{ marginTop: 40 }}></div>
-                  <StyleInput
-                    icon={Images.phone}
-                    placeholder="Phone"
-                    value={phone}
-                    setValue={setPhone}
-                  />
+        <div style={{ marginTop: 40 }}></div>
+        <StyleInput
+          icon={Images.phone}
+          placeholder="Phone"
+          value={phone}
+          setValue={setPhone}
+        />
 
-                  <div style={{ marginTop: 40 }}></div>
-                  <StyleInput
-                    icon={Images.home}
-                    placeholder="Địa chỉ"
-                    value={address}
-                    setValue={setAddress}
-                  />
-                <p><button style={Style.button1} onClick={onChangeInfo}>Cập nhật thông tin</button></p>
-                <button style={Style.button1} onClick={gotoMangerPage}>Quản lý</button>
-            </div>
-            <div style={Style.cartinfo}>
-              <h1>Thông tin khuyến mãi</h1>
-              {listPromotion.map((item) => (
-                <img src={item} style={Style.promotion}/>
-                ))}
-            </div>
-        </div>
-    )
-}
+        <div style={{ marginTop: 40 }}></div>
+        <StyleInput
+          icon={Images.home}
+          placeholder="Địa chỉ"
+          value={address}
+          setValue={setAddress}
+        />
+        <p>
+          <button style={Style.button1} onClick={onChangeInfo}>
+            Cập nhật thông tin
+          </button>
+        </p>
+        <button style={Style.button1} onClick={gotoMangerPage}>
+          Quản lý
+        </button>
+      </div>
+      <div style={Style.cartinfo}>
+        <h1>Thông tin khuyến mãi</h1>
+        {listPromotion.map((item) => (
+          <img src={item} style={Style.promotion} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const Style ={
+const Style = {
   promotion: {
     marginTop: 5,
     marginBottom: 5,
     marginLeft: 5,
     marginRight: 5,
-    width: 340
+    width: 340,
   },
   page: {
     position: "relative",
     width: 1440,
     margin: "auto",
-    
-    display: "flex",
-    flexDirection: "row"
-},
-userinfo: {
-    borderLeft: 130,
-    width: "50%"
-},
-cartinfo: {
-    width: "50%"
-},
 
-button1: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  userinfo: {
+    borderLeft: 130,
+    width: "50%",
+  },
+  cartinfo: {
+    width: "50%",
+  },
+
+  button1: {
     width: 200,
     marginLeft: 125,
     marginTop: 25,
     borderRadius: 50,
-    backgroundColor:"black",
+    backgroundColor: "black",
     height: 50,
     color: "white",
-    fontSize: 20
-}
-}
+    fontSize: 20,
+  },
+};
 
 export default MyPage;
--- client/boy-london/src/navigation/RootScreen.js
+++ client/boy-london/src/navigation/RootScreen.js
@@ -11,7 +11,7 @@ import Instruction from "../feature/user/Instruction";
 import Login from "../feature/user/Login";
 import Policy from "../feature/user/Policy";
 import PolicyHomePage from "../feature/user/PolicyInstruct";
-import Register from "../feature/user/Register";
+import Register from "../feature/user/register";
 import NavBar from "./NavBar";
 import Tracking from "../feature/user/TrackingOrder";
 import { ADMIN_ROUTE, ROOT_SCREEN, SHOP_ROUTE, USER_ROUTE } from "./routes";
@@ -34,7 +34,7 @@ const RootScreen = () => {
         <Route path={USER_ROUTE.instruction} component={Instruction} />
         <Route path={USER_ROUTE.policy} component={Policy} />
         <Route path={USER_ROUTE.policyInstruc} component={PolicyHomePage} />
-        <Route path= {ADMIN_ROUTE.trackingOrder} component={Tracking}/>
+        <Route path={ADMIN_ROUTE.trackingOrder} component={Tracking} />
       </Switch>
     </BrowserRouter>
   );
