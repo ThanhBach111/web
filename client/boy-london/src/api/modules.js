960ed32fd01f68214edd2f8d7053c202466a6740
Duc Dat <phamducdat14122000@gmail.com>
2021-12-11 23:29:30 +0700

    feature/product_detail

--- client/boy-london/src/api/modules.js
+++ client/boy-london/src/api/modules.js
@@ -4,7 +4,7 @@ import request from "./request";
  * Authentication
  */
 export const apiLogin = ({ username, password }) => {
-  console.log(username, ' - ', password)
+  console.log(username, " - ", password);
   const formData = new FormData();
   formData.append("username", username);
   formData.append("password", password);
@@ -41,4 +41,20 @@ export const apiChangeInfo = (params) => {
 
 export const apiGetProfile = () => {
   return request.get("/account");
+};
+
+/**
+ * Product
+ */
+
+//  "productID": 1,
+//  "image1": "https://storage.googleapis.com/cdn.nhanh.vn/store/29193/ps/20211204/21ST_URBAN_Floral_Wool_Overshirt_23.jpg",
+//  "category": "Top",
+//  "price": 200000,
+//  "name": "Áo Armor",
+//  "description": null,
+//  "image2": "https://storage.googleapis.com/cdn.nhanh.vn/store/29193/ps/20211204/21ST_URBAN_Floral_Wool_Overshirt_35.jpg",
+//  "quantityInStock": 100
+export const apiGetProductDetail = (productId) => {
+  return request.get(`/product/get-detail/${productId}`);
 };
