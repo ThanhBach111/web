eb47589896e4cc148d070a43e7811b2f76b11d8e
duongyp23 <71799659+duongyp23@users.noreply.github.com>
2021-12-15 15:23:33 +0700

    Update SearchProduct.js

--- client/boy-london/src/feature/product/SearchProduct.js
+++ client/boy-london/src/feature/product/SearchProduct.js
@@ -3,40 +3,39 @@ import request from "../../api/request";
 
 import Images from "../../assets/images";
 import StyleInput from "../../components/StyleInput";
-
+import StyleProduct from "../../components/StyleProduct";
 
 
 const SearchProduct = () => {
   const [listSearch, setListSearch] = useState([]);
-  const [listProduct, setListProduct] = useState([]);
+  
   const [search, setSearch] = useState("")
  
-
+  
   const getData = async () => {
     try {
      const res = await request.get("/get-landing-page")
-       setListProduct(res);
-      
+       
+       setListSearch(res);
     } catch (err) {
      
       alert(err);
     }
   };
 
-    const searchList = () => {
-        let list = [];
-        listProduct.forEach(item => {
-            if(item.name.toLowerCase().includes(search))
-                list.push(item);
-
-        })
-        setListSearch(list);
+    const searchList = async () => {
+        try {
+          const res = await request.get(`/search/${search}`);
+          setListSearch(res);
+        } catch (err) {
+          
+        }
     }
   
 
   useEffect(() => {
     getData();
-    searchList();
+    
   }, []);
 
   const onGoToDetailProduct = (productID) => {
@@ -48,14 +47,21 @@ const SearchProduct = () => {
     <div style={styles.page}>
       <div style={styles.boxSearch}>
         <h1>TÌM KIẾM SẢN PHẨM</h1>
+        
         <StyleInput
           icon={Images.search}
           placeholder="Search"
           value={search}
           setValue={setSearch}
         />
+        <button style={styles.button} onClick={searchList}>Tìm</button>
+        
       </div>
+      
+      
       <h1>Sản phẩm cần tìm:</h1>
+      <div style={styles.showProduct}>
+        
       {listSearch.map((item) => (
               <StyleProduct
                 image={item.image1}
@@ -65,12 +71,21 @@ const SearchProduct = () => {
                 onPress={() => onGoToDetailProduct(item.productID)}
               />
             ))}
-
+        </div>
     </div>
   );
 };
 
 const styles = {
+  button: {
+    width: 100,
+    marginTop: 25,
+    borderRadius: 50,
+    backgroundColor: "black",
+    height: 50,
+    color: "white",
+    fontSize: 20,
+  },
   page: {
     width: 1400,
     position: "relative",
@@ -81,7 +96,11 @@ const styles = {
       margin: "auto",
       textAlign: "center",
   },
-  
+  showProduct: {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+  }
 };
 
 export default SearchProduct;
