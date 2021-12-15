ac3da9658c317565f04a17c99cc2ac3c3f043122
duongyp23 <71799659+duongyp23@users.noreply.github.com>
2021-12-15 16:20:07 +0700

    Update SearchProduct.js

--- client/boy-london/src/feature/product/SearchProduct.js
+++ client/boy-london/src/feature/product/SearchProduct.js
@@ -4,13 +4,14 @@ import request from "../../api/request";
 import Images from "../../assets/images";
 import StyleInput from "../../components/StyleInput";
 import StyleProduct from "../../components/StyleProduct";
+import { ROOT_SCREEN } from "../../navigation/routes";
 
 
 const SearchProduct = () => {
   const [listSearch, setListSearch] = useState([]);
   
   const [search, setSearch] = useState("");
-  const [alert, setAlert] = useState("");
+  const [alert, setAlert] = useState("Sản phẩm cần tìm:");
  
   
   const getData = async () => {
@@ -29,6 +30,7 @@ const SearchProduct = () => {
           const res = await request.get(`/search/${search}`);
           setListSearch(res);
         } catch (err) {
+          setListSearch([])
           setAlert("Không tìm thấy sản phẩm nào")
         }
     }
@@ -43,6 +45,10 @@ const SearchProduct = () => {
     return productID;
 
   };
+
+  const goToShop = () => {
+    window.location.href = ROOT_SCREEN.shop;
+  }
 
   return (
     <div style={styles.page}>
@@ -60,9 +66,9 @@ const SearchProduct = () => {
       </div>
       
       
-      <h1>Sản phẩm cần tìm:</h1>
+      <h1>{alert}</h1>
       <div style={styles.showProduct}>
-      <h2>{alert}</h2>
+      
       {listSearch.map((item) => (
               <StyleProduct
                 image={item.image1}
@@ -73,6 +79,7 @@ const SearchProduct = () => {
               />
             ))}
         </div>
+        <button style={styles.button2} onClick={goToShop}>xem thêm sản phẩm khác</button>
     </div>
   );
 };
@@ -101,6 +108,14 @@ const styles = {
     width: "100%",
     display: "flex",
     flexWrap: "wrap",
+  },
+  button2: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    
+    marginTop:30,
+    fontSize: "x-large",
+    textDecoration: "underline",
   }
 };
 
